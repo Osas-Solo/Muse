6833d43ac12e62e2fa08e056a520733060c9ace3
Osaremhen Ukpebor <osas.solo@outlook.com>
2022-10-16 20:52:10 +0100

    Added IntroFragment

--- app/src/main/java/com/ostech/muse/IntroFragment.kt
+++ app/src/main/java/com/ostech/muse/IntroFragment.kt
@@ -6,27 +6,33 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
+import com.ostech.muse.databinding.FragmentIntroBinding
 
 class IntroFragment: Fragment() {
-    lateinit var loginButton: AppCompatButton
-    lateinit var signupButton: AppCompatButton
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentIntroBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
+    private lateinit var loginButton: AppCompatButton
+    private lateinit var signupButton: AppCompatButton
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        _binding =
+            FragmentIntroBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
 
-        loginButton = view.findViewById(R.id.intro_login_button)
-        signupButton = view.findViewById(R.id.intro_signup_button)
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
